ef7d38ce53fe0c3e1c19a14741923391853cf490
cvicens <carlos.vicens.alonso@gmail.com>
2019-09-04 11:50:26 +0200

    demo script updated

--- demo-script.sh
+++ demo-script.sh
@@ -1,9 +1,26 @@
-oc tag quay.io/cvicensa/kharon-test:v1.0.0 kharon-operator-tests/kharon-test:v1.0.0 --scheduled=true -n kharon-operator-tests
-oc tag quay.io/cvicensa/kharon-test:v1.1.0 kharon-operator-tests/kharon-test:v1.1.0 --scheduled=true -n kharon-operator-tests
-oc tag quay.io/cvicensa/kharon-test:v1.2.0 kharon-operator-tests/kharon-test:v1.2.0 --scheduled=true -n kharon-operator-tests
+#!/bin/sh
+
+. ./env.sh
+
+oc new-project ${PROJECT_NAME}
+
+for i in {0..2};
+do
+    oc tag quay.io/${QUAY_USERNAME}/kharon-test:v1.${i}.0 kharon-operator-tests/kharon-test:v1.${i}.0 --scheduled=true -n ${PROJECT_NAME}
+done
 
 sleep 10
 
-oc new-app kharon-operator-tests/kharon-test:v1.0.0 --name kharon-test-v1-0-0 -n kharon-operator-tests
-oc new-app kharon-operator-tests/kharon-test:v1.1.0 --name kharon-test-v1-1-0 -n kharon-operator-tests
-oc new-app kharon-operator-tests/kharon-test:v1.2.0 --name kharon-test-v1-2-0 -n kharon-operator-tests
+for i in {0..2};
+do
+    oc new-app kharon-operator-tests/kharon-test:v1.${i}.0 --name kharon-test-v1-${i}-0 -n ${PROJECT_NAME}
+done
+
+oc apply -f ./deploy/role.yaml
+oc apply -f ./deploy/service_account.yaml
+oc apply -f ./deploy/role_binding.yaml
+oc apply -f ./deploy/crds/kharon_v1alpha1_canary_crd.yaml
+
+echo "RUN: oc apply -f ./deploy/crds/kharon_v1alpha1_canary_crd.yaml and modify target to point to kharon-test-v1-0-0, kharon-test-v1-1-0 and kharon-test-v1-2-0"
+echo "When moving to a new version have a look to routes: oc get route -n ${PROJECT_NAME}"
+echo "Also run: oc get route -n ${PROJECT_NAME}"
