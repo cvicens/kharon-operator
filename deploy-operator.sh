778f1ee8fe38ec702ac7ef24f3534c5cef30278d
cvicens <carlos.vicens.alonso@gmail.com>
2019-09-23 22:11:23 +0200

    fixing 'sed' commands to work also in linux

--- deploy-operator.sh
+++ deploy-operator.sh
@@ -9,5 +9,5 @@ oc apply -f ./deploy/role_binding.yaml -n ${PROJECT_NAME}
 oc apply -f ./deploy/crds/kharon_v1alpha1_canary_crd.yaml -n ${PROJECT_NAME}
 
 cat ./deploy/operator.yaml | \
-  sed -E "s/{{\b*QUAY_USERNAME\b*}}/${QUAY_USERNAME}/" | \
-  sed -E "s/{{\b*OPERATOR_VERSION\b*}}/${OPERATOR_VERSION}/" | oc apply -n ${PROJECT_NAME} -f -
+  sed "s/{{\b*QUAY_USERNAME\b*}}/${QUAY_USERNAME}/" | \
+  sed "s/{{\b*OPERATOR_VERSION\b*}}/${OPERATOR_VERSION}/" | oc apply -n ${PROJECT_NAME} -f -
